62f9c7d10d559319e5c9cf957ccd28e563c378dc
Joe <joe@4chen500.com>
2021-03-07 11:39:31 -0500

    write some tests for input button

---
+++ src/components/input-button/input-button.test.js
@@ -0,0 +1,9 @@
+import { render, act } from '@testing-library/react';
+import InputButton from './input-button.js';
+
+describe('InputButton', () => {
+	test('onFormSubmit', () => {
+		// here we will test that the form submits when you hit enter
+		expect(true);
+	});
+});
