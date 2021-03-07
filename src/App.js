3f6ecc40471a38ebc25d981ff1ddbc0cdc0299e5
Joe <joe@4chen500.com>
2021-03-06 19:08:16 -0500

    add input-button mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AppButton from './components/app-button';
+import InputButton from './controls/input-button';
 
 function App() {
 	return (
 		<div className="App">
 			<div className="component-test-area">
-				<AppButton label="+ Topic" onClick={() => console.log('AppButton Click')} />
+				<AppButton label="This be a button" onClick={() => console.log('AppButton Click')} />
+				<InputButton label="+ Topic" />
 			</div>
 		</div>
 	);
